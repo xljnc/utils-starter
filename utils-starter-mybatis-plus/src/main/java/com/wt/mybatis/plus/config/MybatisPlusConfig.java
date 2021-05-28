1635571364c6e0f43af918ac2e6003371bd42d80
yipin.wt <yipin.wt@cfuture.shop>
2021-05-28 15:58:57 +0800

    mybatis plus starter by qiyu 2021-05-28

--- utils-starter-mybatis-plus/src/main/java/com/wt/mybatis/plus/config/MybatisPlusConfig.java
+++ utils-starter-mybatis-plus/src/main/java/com/wt/mybatis/plus/config/MybatisPlusConfig.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerIntercept
 import com.wt.mybatis.plus.injector.BatchSqlInjector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +35,7 @@ public class MybatisPlusConfig {
 
     @Bean
     @ConditionalOnMissingBean(BatchSqlInjector.class)
+    @ConditionalOnExpression("${mybatis-plus.dbType:mysql}")
     public ISqlInjector batchSqlInjector() {
         return new BatchSqlInjector();
     }
